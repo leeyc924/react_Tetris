16484fe54cd7282ef4a2b2d7207e9241647c8e7a
leeyc924 <leeyc924@naver.com>
2022-01-12 22:51:04 +0900

    t

--- hooks/usePlayer.js
+++ hooks/usePlayer.js
@@ -5,7 +5,7 @@ import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 const usePlayer = () => {
   const [player, setPlayer] = useState({
-    pos: { x: 0, y: 0 },
+    pos: { x: 0, y: 0 }, 
     tetromino: TETROMINOS[0].shape,
     collided: false,
   });
